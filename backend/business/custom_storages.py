bee59231f8f8657d6efa2f22ddc6767130e743a7
Sayan Karmakar <ironheartz367@gmail.com>
2024-06-26 14:32:14 +0530

    Update custom_storages.py

--- backend/business/custom_storages.py
+++ backend/business/custom_storages.py
@@ -1,7 +1,7 @@
-# from storages.backends.s3boto3 import S3Boto3Storage
+from storages.backends.s3boto3 import S3Boto3Storage
 
-# class CustomS3Boto3Storage(S3Boto3Storage):
-#     def __init__(self, *args, **kwargs):
-#         bucket_name = 'django-storge-app'
-#         kwargs['default_acl'] = 'public-read'  # Set ACL to public-read
-#         super().__init__(*args, **kwargs)
+class CustomS3Boto3Storage(S3Boto3Storage):
+    def __init__(self, *args, **kwargs):
+        bucket_name = 'django-storge-app'
+        kwargs['default_acl'] = 'public-read'  # Set ACL to public-read
+        super().__init__(*args, **kwargs)
